333af78688f8bf618a3fcbb283373d660fce148e
Aljon Mendiola <aljon_mendiola799224@yahoo.com>
2021-12-02 21:31:53 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e42c1a51b101802af5216aa506e1f46f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'afb6cd9e-65cb-4ed1-b925-ac3a00417f4c'
+};
